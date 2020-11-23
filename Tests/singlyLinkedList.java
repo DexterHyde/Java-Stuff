3bc439f27439fbfe008bc2ba806a89689a55b813
Diego <dextersao98@gmail.com>
2020-11-22 19:03:59 -0600

    singlyLinked List

---
+++ Tests/singlyLinkedList.java
@@ -0,0 +1,41 @@
+package Tests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class singlyLinkedList {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(-129);
+        head.next = new ListNode(-129);
+
+        System.out.println(isPalindrome(head));
+    }
+
+    public static boolean isPalindrome(ListNode head){
+        ListNode by = head;
+        List <Integer> nums = new ArrayList<Integer>();
+        while(by != null){
+            nums.add(by.val);
+            by = by.next;
+        }
+
+        int start = 0, end = nums.size() -1;
+
+        while(end >= start){
+            if(!nums.get(start).equals(nums.get(end))){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static class ListNode{
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {this.val = val;}
+        ListNode(int val, ListNode next) {this.val = val; this.next = next;}
+    }
+}
